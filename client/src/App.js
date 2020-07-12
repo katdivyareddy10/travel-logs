81160dfa8a7c0e174ede483e5e42f05e75bb69c9
Divya Katpally <katdivyareddy@gmail.com>
2020-07-12 11:16:43 -0500

    Front end added

---
+++ client/src/App.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+
+import { listLogEntries } from "./Api";
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
+  const [viewport, setViewport] = useState({
+    latitude: 37,
+    longitude: -95,
+    zoom: 4,
+    bearing: 0,
+    pitch: 0,
+  });
+
+  useEffect(() => {
+    //IFFE - immediately invoked function expression to support async calls in useEffect()
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+      console.log(logEntries);
+    })();
+  }, []);
+
+  const showAddMarkerPopup = (event) => {
+    const [longitude, latitude] = event.lngLat;
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    });
+    console.log(event);
+  };
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      width="100vw"
+      height="100vh"
+      mapStyle="mapbox://styles/mapbox/dark-v9"
+      onDblClick={showAddMarkerPopup}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    >
+      {logEntries.map((entry) => (
+        <>
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+          >
+            <div onClick={() => setShowPopup({ [entry._id]: true })}>
+              <img
+                className="marker"
+                style={{
+                  height: `${5 * viewport.zoom}px`,
+                  width: `${5 * viewport.zoom}px`,
+                }}
+                src="https://i.imgur.com/y0G5YTX.png"
+                alt="Location Pin"
+              />
+            </div>
+          </Marker>
+          {showPopup[entry._id] ? (
+            <Popup
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setShowPopup({})}
+              anchor="top"
+            >
+              <div className="popup">
+                <h3>{entry.title}</h3>
+                <p>{entry.comments}</p>
+                <small>
+                  Visited on {new Date(entry.visitDate).toLocaleDateString()}
+                </small>
+              </div>
+            </Popup>
+          ) : null}
+        </>
+      ))}
+
+      {addEntryLocation ? (
+        <>
+          <Marker
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+          >
+            <div>
+              <img
+                className="marker"
+                style={{
+                  height: `${5 * viewport.zoom}px`,
+                  width: `${5 * viewport.zoom}px`,
+                }}
+                src="https://www.freepnglogos.com/uploads/pin-png/map-pin-png-apex-dance-studio-15.png"
+                alt="Location Pin"
+              />
+            </div>
+          </Marker>
+          <Popup
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setShowPopup({})}
+            anchor="top"
+          >
+            <div className="popup">Add your new log entry here!</div>
+          </Popup>
+        </>
+      ) : null}
+    </ReactMapGL>
+  );
+};
+export default App;
